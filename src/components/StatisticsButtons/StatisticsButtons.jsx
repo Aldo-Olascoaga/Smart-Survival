5288129e962f93f65f1572b5a0b0236abc3a6229
= <=>
2023-05-02 10:59:29 -0600

    History mode change

--- src/components/StatisticsButtons/StatisticsButtons.jsx
+++ src/components/StatisticsButtons/StatisticsButtons.jsx
@@ -9,9 +9,7 @@ const StatisticsButtons = () => {
   const navigate = useNavigate();
 
   const goToHistoriaModoHistoria = () => {
-    navigate(
-      'https://front-end-test.d3baae27c9vcp4.amplifyapp.com/statistics/historia/mode/historia'
-    );
+    navigate('/statistics/historia/mode/historia');
   };
 
   const goToHistoriaModoOnline = () => {
